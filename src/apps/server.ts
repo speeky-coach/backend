b914363b3dc84c10bb3a46338744c7232e410c79
Grover Lee <bruce.lee@encora.com>
2022-10-07 20:37:50 -0500

    add express setup

--- src/apps/server.ts
+++ src/apps/server.ts
@@ -1,8 +1,16 @@
-import 'module-alias/register';
 import 'dotenv/config';
 import packageJson from '../../package.json';
-import { logger } from '@speeky/framework';
+import { ExpressApp, logger } from '../framework';
 
 const appVersion = packageJson.version;
 
 logger.info('version ' + appVersion);
+
+const expressApp = new ExpressApp(packageJson.version, [], []);
+expressApp.applyErrorManagement();
+
+if (process.env.NODE_ENV !== 'test') {
+  expressApp.start([]);
+}
+
+export { expressApp };
--- src/framework/express/ExpressApp.ts
+++ src/framework/express/ExpressApp.ts
@@ -68,7 +68,7 @@ class ExpressApp {
   public listen(): void {
     if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'test.local') {
       this.app.listen(process.env.PORT, () => {
-        logger.info(`Connected [port ${process.env.PORT}]`);
+        logger.info(`Express App Connected [port ${process.env.PORT}]`);
       });
     }
   }
